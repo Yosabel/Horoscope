e95e6f4b3d83cd1af1784555aa1b435a9bc6f153
yosabeltekeste <yosabeltekeste@students.berkeley.net>
2017-11-01 17:12:08 -0700

    hi

--- script.js
+++ script.js
@@ -30,27 +30,41 @@ function daysAvailable(month){
 }
 
 function determineSign(month,day) {
-    if((month==1 && day<=19)|| (month==12 && day<=15)){
-        return "Capricorn";
+    if((month==1 && day<=19)|| (month==12 && day>=22)){
+        return " You are a Capricorn!";
     }
-    if((month==2 && day)){
-
+    if((month==1 && day>=20) || (month==2 && day<=18) ){
+        return "You are an Aquarius! "
 
     }
-
-
-
-
-
-    //
-    // }
-    // if(month==2){
-    //     if(day<=18){
-    //         return "aquarius";
-    //     }
-    //     return "Pisces";
-    // }
+    if((month==2 && day>=19)|| (month==3 && day<=20)){
+        return " You are a Pisces!";
+    }
+    if((month==3 && day>=21)|| (month==4 && day<=19)){
+        return " You are an Aries!";
+    }
+    if((month==4 && day<=20)|| (month==5 && day<=20)){
+        return " You are a Taurus!";
+    }
+    if((month==5 && day>=21)|| (month==6 && day<=20)){
+        return " You are a Gemini!";
+    }
+    if((month==6 && day>=21)|| (month==7 && day<=22)){
+        return " You are a Cancer!";
+    }
+    if((month==7 && day>=23)|| (month==8 && day<=22)){
+        return " You are a Leo!";
+    }
+    if((month==8 && day>=23)|| (month==9 && day<=22)){
+        return " You are a Virgo!";
+    }
+    if((month==9 && day>=23)|| (month==10 && day<=22)){
+        return " You are a Libra!";
+    }
+    if((month==10 && day>=23)|| (month==11 && day<=21)){
+        return " You are a Scorpio!";
+    }
+    if((month==11 && day>=22)|| (month==12 && day<=21)){
+        return " You are a Sagittarius!";
+    }
 }
-
-
-
